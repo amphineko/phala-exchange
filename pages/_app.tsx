eec34bce88eaf91061f5e90d9d7d62925657527f
amphineko <sukareki@gmail.com>
2021-03-15 15:29:08 +0800

    feat(app): add multiple or zero accounts handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,47 @@ import { Box, Container, CssBaseline, Paper, Tab, Tabs } from '@material-ui/core
 import { AppProps } from 'next/app'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Web3 from 'web3'
 import Web3Context from '../contexts/Web3Context'
 import { useWeb3 } from '../providers/web3'
 import WalletWidget from '../widgets/Wallet'
+
+function useWeb3Accounts(web3: Web3 | null): {
+    accounts: string[] | null
+    currentAccount: string | null
+    setCurrentAccount: (account: string) => void
+} {
+    const [accounts, setAccounts] = useState<string[] | null>(null)
+    const [account, setCurrentAccount] = useState<string | null>(null)
+
+    useEffect(() => {
+        if (web3 === null) {
+            setAccounts(null)
+            return
+        }
+
+        web3.eth.getAccounts().then((accounts) => {
+            if (accounts instanceof Array) {
+                setAccounts(accounts)
+                setCurrentAccount(accounts[0] ?? null) // TODO: remember last selection
+            } else {
+                console.log(accounts)
+                throw new Error('getAccounts() returned non-array')
+            }
+        }, (reason) => {
+            setAccounts([])
+            console.error(`Failed to retrieve accounts: ${reason as string}`)
+            // TODO: display account retrieve failure on UI
+        })
+    }, [web3])
+
+    return {
+        accounts,
+        currentAccount: account,
+        setCurrentAccount
+    }
+}
 
 export default function Application({ Component, pageProps }: AppProps): JSX.Element {
     const {
@@ -19,6 +55,12 @@ export default function Application({ Component, pageProps }: AppProps): JSX.Ele
     const web3 = useMemo(() => {
         return web3provider === null ? null : new Web3(web3provider as any)
     }, [web3provider])
+
+    const {
+        accounts: availableAccounts,
+        currentAccount: account,
+        setCurrentAccount: setAccount
+    } = useWeb3Accounts(web3)
 
     const router = useRouter()
 
@@ -34,7 +76,14 @@ export default function Application({ Component, pageProps }: AppProps): JSX.Ele
                 <Paper elevation={4}>
                     <Box component="header" style={{ padding: '1rem', paddingBottom: 0 }}>
                         <h1 style={{ fontSize: '1.2rem', fontWeight: 'normal' }}>tPHA exchange</h1>
-                        <WalletWidget connect={connectWeb3} currentState={web3state} disconnect={disconnectWeb3} web3={web3} />
+                        <WalletWidget
+                            availableAccounts={availableAccounts}
+                            connect={connectWeb3}
+                            currentAccount={account}
+                            currentState={web3state}
+                            disconnect={disconnectWeb3}
+                            setAccount={setAccount}
+                        />
                     </Box>
                     <Box>
                         <Tabs value={currentTabIndex}>
--- widgets/Wallet.tsx
+++ widgets/Wallet.tsx
@@ -2,8 +2,7 @@ import { Box, Button, CircularProgress } from '@material-ui/core'
 import EjectIcon from '@material-ui/icons/Eject'
 import LockOpenIcon from '@material-ui/icons/LockOpen'
 import { Alert } from '@material-ui/lab'
-import React, { useEffect, useMemo, useState } from 'react'
-import Web3 from 'web3'
+import React, { useMemo } from 'react'
 import { Web3State } from '../providers/web3'
 
 export interface Provider {
@@ -12,50 +11,23 @@ export interface Provider {
 
 export default function WalletWidget(props: {
     connect: () => void
-    currentState: Web3State
     disconnect: () => void
-    web3: Web3 | null
+    setAccount: (account: string) => void
+
+    currentState: Web3State
+
+    availableAccounts: string[] | null
+    currentAccount: string | null
 }): JSX.Element {
-    const { connect, currentState: state, disconnect, web3 } = props
+    // TODO: implement account selector
 
-    const [account, setAccount] = useState<string | null>(null)
-    // const [balance, setBalance] = useState<string | null>(null)
-
-    useEffect(() => {
-        if (web3 === null) {
-            setAccount(null)
-            // setBalance(null)
-            return
-        }
-
-        web3.eth.getAccounts().then((accounts) => {
-            if (typeof accounts[0] !== 'string') {
-                throw new Error('No accounts connected')
-            }
-
-            setAccount(accounts[0])
-        }).catch((reason) => {
-            console.error(`Failed to read accounts: ${reason as string}`)
-            // TODO: better UI presentation of account retrieve failure
-
-            setAccount(null)
-            throw reason
-        })
-    }, [web3])
-
-    // useEffect(() => {
-    //     if (web3 === null || account === null) { return }
-
-    //     web3.eth.getBalance(account).then((balance) => {
-    //         setBalance(`${parseInt(balance) / 1000000000000000000}`)
-    //     }, (reason) => {
-    //         console.error(`Failed to retrieve account balance: ${reason as string}`)
-    //         // TODO: better UI presentation of balance retrieve failure
-
-    //         setBalance('FAILED')
-    //         throw reason
-    //     })
-    // })
+    const {
+        // availableAccounts: accounts,
+        connect,
+        currentAccount: account,
+        currentState: state,
+        disconnect
+    } = props
 
     const button = useMemo(() => {
         switch (state) {
@@ -79,14 +51,19 @@ export default function WalletWidget(props: {
         }
     }, [connect, disconnect, state])
 
-    const accountInfo = useMemo(() => account !== null
-        ? (<Alert severity='success'>Account: {account}</Alert>)
-        : null, [account])
+    const accountInfo = useMemo(() =>
+        state === 'connected'
+            ? (account !== null
+                ? (<Alert severity='success'>Account: {account}</Alert>)
+                : (<Alert severity='error'>No account available</Alert>)
+            )
+            : null, [account, state])
 
     return (
         <Box>
             <Box display="block">{button}</Box>
-            <Box display="block" style={{ marginTop: '1rem' }}>{accountInfo}</Box>
+            {state === 'connected' &&
+                <Box display="block" style={{ marginTop: '1rem' }}>{accountInfo}</Box>}
         </Box>
     )
 }
