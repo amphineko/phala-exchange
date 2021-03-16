b144835f487b4a2af9e19747728b7467b3a1aec7
amphineko <sukareki@gmail.com>
2021-03-16 11:30:39 +0800

    refactor(burn): improved code
    extracted burn logic
    extracted error presentation widgets

--- pages/burn.tsx
+++ pages/burn.tsx
@@ -2,11 +2,12 @@ import { Button, CircularProgress, Container, Link, TextField, Typography } from
 import MonetizationOn from '@material-ui/icons/MonetizationOn'
 import { Alert } from '@material-ui/lab'
 import React, { useContext, useMemo, useState } from 'react'
+import Web3 from 'web3'
 import Web3Context from '../contexts/Web3Context'
 import { load as loadContract } from '../lib/phalaTokenContract'
 
 const burnToAddress = '0x000000000000000000000000000000000000dead'
-const burnAmountHelperText = 'NOTE: The maximum amount to exchange is limited to 0.1 to avoid accidental asset loss. Ask in our Discord if you want to exchange more.'
+const burnAmountHelperText = ''
 const defaultAmount = 0.1
 
 interface RecentTransaction {
@@ -14,15 +15,32 @@ interface RecentTransaction {
     hash: string
 }
 
+async function burn(account: string, amount: Number, web3: Web3): Promise<RecentTransaction> {
+    const { consts, contract } = await loadContract(web3)
+
+    const wei = web3.utils.toWei(amount.toString())
+
+    const receipt = await contract.methods
+        .transfer(burnToAddress, wei)
+        .send({ from: account })
+
+    return {
+        etherscanBaseUrl: consts.etherscanBaseUrl,
+        hash: receipt.transactionHash
+    }
+}
+
 export default function BurnPage(): JSX.Element {
     const { account, web3 } = useContext(Web3Context)
+
+    const [isBurning, setBurning] = useState<boolean>(false)
 
     // amount input by user and validations
 
     const [amount, setAmount] = useState<Number | null>(defaultAmount)
     const [amountError, setAmountError] = useState<string | null>(null)
 
-    const onAmountChange = (input: string): void => {
+    const onAmountInputChanged = (input: string): void => {
         const value = Number(input)
         setAmount(null)
 
@@ -39,23 +57,9 @@ export default function BurnPage(): JSX.Element {
         setAmountError(null)
     }
 
-    // recent transactions
+    // local conditions preventing the exchange to proceed
 
-    const [lastTxnError, setLastTxnError] = useState<string | null>(null)
-    const [lastTxn, setLastTxn] = useState<RecentTransaction | null>(null)
-    const lastTxnInspectUrl = useMemo(
-        () => lastTxn !== null
-            ? `${lastTxn.etherscanBaseUrl}/tx/${lastTxn.hash}`
-            : null
-        , [lastTxn])
-
-    // active transtion in progress
-
-    const [active, setActive] = useState<boolean>(false)
-
-    // error preventing the exchange to proceed
-
-    const error = useMemo<string | null>(() => {
+    const localError = useMemo<string | null>(() => {
         if (web3 === null) return 'Ethereum wallet is not connected'
         if (account === null) return 'No Ethereum account selected'
 
@@ -65,40 +69,62 @@ export default function BurnPage(): JSX.Element {
     // allow user to proceed or not
 
     const enabled = useMemo<boolean>(
-        () => web3 !== null && account !== null && amount !== null && !active,
-        [web3, account, amount, active]
+        () => web3 !== null && account !== null && amount !== null && !isBurning,
+        [web3, account, amount, isBurning]
     )
 
-    // burn logic
+    // recent transactions
 
-    const burn = async (): Promise<void> => {
-        if (active || web3 === null || account === null || amount === null) return
+    const [lastTxnError, setLastTxnError] = useState<string | null>(null)
+    const [lastTxn, setLastTxn] = useState<RecentTransaction | null>(null)
 
+    // button handler
+
+    const startBurn = (): void => {
+        setBurning(true)
         setLastTxn(null)
 
-        const { consts, contract } = await loadContract(web3)
-        const wei = web3.utils.toWei(amount.toString())
+        if (account === null || amount === null || web3 === null) {
+            return
+        }
 
-        console.log(`Sending ${amount.toString()} PHA (${wei} wei)`)
-
-        const receipt = await contract.methods
-            .transfer(burnToAddress, wei)
-            .send({ from: account })
-
-        setLastTxn({
-            etherscanBaseUrl: consts.etherscanBaseUrl,
-            hash: receipt.transactionHash
-        })
+        burn(account, amount, web3)
+            .then((txn) => setLastTxn(txn))
+            .catch((reason) => setLastTxnError(reason))
+            .finally(() => setBurning(false))
     }
 
-    const onClickBurn = (): void => {
-        setActive(true)
-        burn().then(() => { }, (error) => {
-            setLastTxnError(error.message ?? error)
-        }).finally(() => {
-            setActive(false)
-        })
-    }
+    // error presentation widget
+
+    const localConditionErrorWidget = useMemo(() => localError === null || (
+        <Alert severity="error" style={{ marginTop: '1rem' }}>
+            <span style={{ wordBreak: 'break-all', wordWrap: 'break-word' }}>
+                {localError}
+            </span>
+        </Alert>
+    ), [localError])
+
+    const lastTxnErrorWidget = useMemo(() => lastTxnError === null || (
+        <Alert severity="error" style={{ marginTop: '1rem' }}>
+            <span style={{ wordBreak: 'break-all', wordWrap: 'break-word' }}>
+                Transaction failed: {lastTxnError}
+            </span>
+        </Alert>
+    ), [lastTxnError])
+
+    const lastTxnInfoWidget = useMemo(() => {
+        if (lastTxn === null) return false
+
+        const txnInspectUrl = `${lastTxn.etherscanBaseUrl}/tx/${lastTxn.hash}`
+
+        return (
+            <Alert severity="success" style={{ marginTop: '1rem' }}>
+                <span style={{ wordBreak: 'break-all', wordWrap: 'break-word' }}>
+                    Transaction sent: <Link href={txnInspectUrl} rel="noreferrer" target="_blank">{lastTxn.hash}</Link>
+                </span>
+            </Alert >
+        )
+    }, [lastTxn])
 
     return (
         <Container>
@@ -108,42 +134,31 @@ export default function BurnPage(): JSX.Element {
                 fullWidth
                 helperText={amountError === null ? burnAmountHelperText : amountError}
                 label="Amount"
-                onChange={(ev) => onAmountChange(ev.target.value)}
+                onChange={(ev) => onAmountInputChanged(ev.target.value)}
                 required
             />
+
             <Button
                 disabled={!enabled}
-                onClick={onClickBurn}
-                startIcon={!active ? <MonetizationOn /> : <CircularProgress />}
+                onClick={() => startBurn()}
+                startIcon={!isBurning ? <MonetizationOn /> : <CircularProgress />}
                 style={{ marginTop: '1rem' }}
                 variant="contained"
             >Exchange</Button>
-            {error === null || (
-                <Alert severity="error" style={{ marginTop: '1rem' }}>
-                    <span style={{ wordBreak: 'break-all', wordWrap: 'break-word' }}>
-                        {error}
-                    </span>
-                </Alert>
-            )}
-            {lastTxnError !== null && (
-                <Alert severity="error" style={{ marginTop: '1rem' }}>
-                    <span style={{ wordBreak: 'break-all', wordWrap: 'break-word' }}>
-                        Transaction failed: {lastTxnError}
-                    </span>
-                </Alert>
-            )}
-            {lastTxn !== null && (
-                <Alert severity="success" style={{ marginTop: '1rem' }}>
-                    <span style={{ wordBreak: 'break-all', wordWrap: 'break-word' }}>
-                        Transaction sent: <Link href={lastTxnInspectUrl ?? '#'} rel="noreferrer" target="_blank">{lastTxn.hash}</Link>
-                    </span>
-                </Alert>
-            )}
+
+            {localConditionErrorWidget}
+
+            {lastTxnErrorWidget}
+
+            {lastTxnInfoWidget}
+
             <Typography
                 color='textSecondary'
-                hidden={!(!active || lastTxn === null)}
+                hidden={!(!isBurning || lastTxn === null)}
                 style={{ marginTop: '1rem' }}
                 variant="body2">
+                The maximum amount to exchange is limited to 0.1 to avoid accidental asset loss. <br />
+                Ask in our Discord if you want to exchange more. <br />
                 Please sit back and relax while the transaction is being sent and accepted by the network.
             </Typography>
         </Container >
