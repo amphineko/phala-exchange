babde78e227678ad8d8f7fe17d4ee63d12e6753f
amphineko <sukareki@gmail.com>
2021-03-18 16:23:28 +0800

    chore: move to poc4 net
    Signed-off-by: amphineko <sukareki@gmail.com>

--- lib/phala/phaNetworks.ts
+++ lib/phala/phaNetworks.ts
@@ -6,11 +6,11 @@ export interface NetworkDescription {
 export const Networks: Record<string, NetworkDescription> = {
     main: {
         inspectTxUrl: (hash) => `https://poc3.phala.network/polkadotjs/#/explorer/query/${hash}`,
-        websocketEndpoint: 'wss://poc3.phala.network/ws'
+        websocketEndpoint: 'wss://poc4.phala.network/ws'
     },
     test: {
         inspectTxUrl: (hash) => `https://poc3.phala.network/polkadotjs/#/explorer/query/${hash}`,
-        websocketEndpoint: 'wss://poc3.phala.network/ws'
+        websocketEndpoint: 'wss://poc4.phala.network/ws'
     },
     localhost: {
         inspectTxUrl: (hash) => `http://localhost:9944/polkadotjs/#/explorer/query/${hash}`,
